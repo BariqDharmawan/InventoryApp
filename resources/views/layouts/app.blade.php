7cffc2b2ecf0374bcd9590c9257cc4d0c837d0e5
fathrahh <gazoon3@gmail.com>
2023-08-02 09:06:28 +0700

    sign out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,18 +87,23 @@
                         <span class="ml-3 flex-1 whitespace-nowrap">Procurement</span>
                     </a>
                 </li>
-                <li>
-                    <a class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        href="#">
-                        <svg aria-hidden="true"
-                            class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                            fill="none" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" />
-                        </svg>
-                        <span class="ml-3 flex-1 whitespace-nowrap">Sign Out</span>
-                    </a>
-                </li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <li>
+                        <button
+                            class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 "
+                            type="submit">
+                            <svg aria-hidden="true"
+                                class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                                fill="none" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    stroke="currentColor" />
+                            </svg>
+                            <span class="ml-3 flex-1 whitespace-nowrap">Sign Out</span>
+                        </button>
+                    </li>
+                </form>
             </ul>
         </div>
     </aside>
